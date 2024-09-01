3c1d6e29db404e0956879741aa86460fd2d5cad7
nishanpradhan <nishanpradhan500@gmail.com>
2024-09-01 21:45:41 +0545

    laravel-initial

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Freelance</title>
+</head>
+
+<body>
+    <h1>Hell0 me Welcome Freelancing</h1>
+    <p>Freelancing is a way of working where you offer your skills and services to
+        clients on a project-by-project basis. You are essentially your own boss and
+        work on a variety of projects for different clients. Freelancing can be a great
+        way to earn a living, especially if you have a skill that is in high demand.
+    </p>
+
+    <a href="/about">About</a>
+    <a href="/nishan/isha">nishan</a>
+    <a href="/services">services</a>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/services', function () {
+    return view('services');
+});
+Route::get('/nishan/{name}', function ($name) {
+    // echo $name;
+    return view('nishan', ['name' => $name]);
 });
