71b10b0fce1ded3395dc096d47e0b244a8949920
nishanpradhan <nishanpradhan500@gmail.com>
2024-09-21 11:01:28 +0545

    learn-links,components

--- resources/views/edit-students.blade.php
+++ resources/views/edit-students.blade.php
@@ -1,61 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>STD</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">Nishan Pradhan</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/view">View</a>
-                </li>
-
-
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
-    </nav>
-
-
-    <form action="/update/{{$student->id}}" method="post" class="p-4">
-        <!-- generate unique token -->
-        @csrf
-        <h1>Edit Students Details</h1>
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" class="form-control" id="name" name="name" aria-describedby="name" placeholder="Name" value="{{$student->name}}">
-        </div>
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" class="form-control" id="email" name="email" aria-describedby="email" placeholder="Email" value="{{$student->email}}">
-        </div>
-        <div class="form-group">
-            <label for="contact">Contact</label>
-            <input type="tel" class="form-control" id="contact" name="contact" aria-describedby="contact" placeholder="Contact" value="{{$student->contact}}">
-        </div>
-        <button type="submit" class="btn btn-primary">Update</button>
-    </form>
-</body>
-
-</html>
+@extends('layout.main')
+@section('title','Edit Students')
+@section('content')
+<form action="/update/{{$student->id}}" method="post" class="p-4">
+    <!-- generate unique token -->
+    @csrf
+    <h1>Edit Students Details</h1>
+    <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" id="name" name="name" aria-describedby="name" placeholder="Name" value="{{$student->name}}">
+    </div>
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" id="email" name="email" aria-describedby="email" placeholder="Email" value="{{$student->email}}">
+    </div>
+    <div class="form-group">
+        <label for="contact">Contact</label>
+        <input type="tel" class="form-control" id="contact" name="contact" aria-describedby="contact" placeholder="Contact" value="{{$student->contact}}">
+    </div>
+    <button type="submit" class="btn btn-primary">Update</button>
+</form>
+@endsection
--- resources/views/view-student.blade.php
+++ resources/views/view-student.blade.php
@@ -1,72 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>View Student</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">Nishan Pradhan</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/view">View</a>
-                </li>
-
-
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
-    </nav>
-    <div class="p-4 ">
-        <h1>Student Data</h1>
-        <h4>Total Students : {{$students->count()}}</h4>
-        <table class="table">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Contact</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php $i = 0; ?>
-                @foreach($students as $student)
-                <tr>
-                    <th scope="row">{{++$i}}</th>
-                    <td>{{$student->name}}</td>
-                    <td>{{$student->email}}</td>
-                    <td>{{$student->contact}}</td>
-                    <td>
-                        <a href="/edit/{{$student->id}}" class="btn btn-primary">Edit</a>
-                        <a href="/delete/{{$student->id}}" class="btn btn-danger">Delete</a>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-
-</body>
-
-</html>
+@extends('layout.main')
+@section('title','View Students')
+@section('content')
+<div class="p-4 ">
+    <h1>Student Data</h1>
+    <h4>Total Students : {{$students->count()}}</h4>
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Contact</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $i = 0; ?>
+            @foreach($students as $student)
+            <tr>
+                <th scope="row">{{++$i}}</th>
+                <td>{{$student->name}}</td>
+                <td>{{$student->email}}</td>
+                <td>{{$student->contact}}</td>
+                <td>
+                    <a href="/edit/{{$student->id}}" class="btn btn-primary">Edit</a>
+                    <a href="/delete/{{$student->id}}" class="btn btn-danger">Delete</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
