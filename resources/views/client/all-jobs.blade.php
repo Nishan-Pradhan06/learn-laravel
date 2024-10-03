e2f68c86e27b17f08017b11bae393cfb8d460caf
nishanpradhan <nishanpradhan500@gmail.com>
2024-10-03 12:58:39 +0545

    fix-padding

--- resources/views/client/all-jobs.blade.php
+++ resources/views/client/all-jobs.blade.php
@@ -1,27 +1,35 @@
 @extends('layout.client')
 @section('title','Home')
 @section('client-content')
-<div class="px-5">
-    <br>
-    <h3>All Jobs</h3>
+<style>
+    .container {
+        padding: 20px 10px;
+    }
+</style>
+<div class="container">
 
-    @foreach($jobs as $job)
-    <div class="card mb-4">
-        <div class="card-header">
-            <p style="font-size: 10px;">Posted {{ \Carbon\Carbon::parse($job->created_at)->diffForHumans() }}</p>
-            <h5>{{$job->title}}</h5>
+    <div class="px-5">
+        <br>
+        <h3>All Jobs</h3>
+
+        @foreach($jobs as $job)
+        <div class="card mb-4">
+            <div class="card-header">
+                <p style="font-size: 10px;">Posted {{ \Carbon\Carbon::parse($job->created_at)->diffForHumans() }}</p>
+                <h5>{{$job->title}}</h5>
+            </div>
+            <div class="card-body">
+                <p class="card-text">{!! str_replace(['{', '}'], '', $job->des) !!}</p>
+                <p class="card-text">Duration: {{$job->duration}}</p>
+                <p class="card-text">Budget: Rs. {{$job->budget}}</p>
+                <p class="card-text">Tech Stack: {{$job->req}}</p>
+                <a href="{{ url('/edit-work/' . $job->id) }}" class="btn btn-primary">Edit</a>
+                <a href="{{ url('/delete/' . $job->id) }}" class="btn btn-danger">Delete</a>
+            </div>
         </div>
-        <div class="card-body">
-            <p class="card-text">{!! str_replace(['{', '}'], '', $job->des) !!}</p>
-            <p class="card-text">Duration: {{$job->duration}}</p>
-            <p class="card-text">Budget: Rs. {{$job->budget}}</p>
-            <p class="card-text">Tech Stack: {{$job->req}}</p>
-            <a href="{{ url('/edit-work/' . $job->id) }}" class="btn btn-primary">Edit</a>
-            <a href="{{ url('/delete/' . $job->id) }}" class="btn btn-danger">Delete</a>
-        </div>
+        @endforeach
     </div>
-    @endforeach
-</div>
 
+</div>
 
 @endsection
